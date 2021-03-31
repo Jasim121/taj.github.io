1909745a259a89a633ba108929bc9e39a5b9064f
Jasim Gazi <Jasimakram96@gmail.com>
2021-03-31 20:21:12 +0530

    taj website

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
